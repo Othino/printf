0c8e5cffa62f9217dd1056525558c9329c3cad1e
Othino <micahothino44@gmail.com>
2022-08-07 23:34:54 -0700

    header file

--- main.h
+++ main.h
@@ -3,6 +3,17 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_c(va_list c);
